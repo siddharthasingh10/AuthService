f3b5c8aff5718daf81dcc80e716551bae6879b5f
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-10-03 05:19:55 +0530

    created token and verify function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
-const UserRepository=require('../repository/user-repository')
+const jwt=require('jsonwebtoken');
+const {JWT_KEY}=require('../config/serverconfig')
+const UserRepository=require('../repository/user-repository');
+
 
 class UserService{
     constructor(){
@@ -36,5 +39,28 @@ class UserService{
             throw {error}
         }
     }
+    createToken(user){
+        try{
+            const token=jwt.sign(user,JWT_KEY,{expiresIn:'1d'})
+            return token;
+        }catch(error){
+            console.log('something wrong in creating token')
+            throw error;
+
+        }
+       
+    }
+    verifyToken(token){
+        try{
+                const response=jwt.verify(token,JWT_KEY);
+                return response;
+        }
+        catch(error){
+            console.log('soething wrong in verifying token',error)
+
+        }
+
+    }
 }
+
 module.exports=UserService;
