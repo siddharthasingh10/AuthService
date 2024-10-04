2d4c19683a8f988fdfc4938df3e65c7a3d079b8e
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-10-04 23:23:50 +0530

    added role model

--- src/middlware/auth-request-validators.js
+++ src/middlware/auth-request-validators.js
@@ -1,6 +1,6 @@
 const validateUserAuth=(req,res,next)=>{
 // same middleware can be used for the signup or the login
-    if(req.body.email || req.body.password){
+    if(!req.body.email || !req.body.password){
         return res.status(400).json({
             success:false,
             data:{},
